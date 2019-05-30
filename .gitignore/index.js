b5b69c6c45c15419c37bf9a791dbcd6882cae2ee
Poolp34 <50843034+Poolp34@users.noreply.github.com>
2019-05-30 11:03:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,5 +5,77 @@ var prefix = "!"
 client.login("NTc5MjMwMTA3MTUxODkyNDgw.XOEsUA.bHnUizomEWkzWzjCTdYJhOCsNzo");
 
 client.on("message", async (message) => {
+    if (message.author.bot) return
+    if (!message.content.startsWith(prefix)) return;
 
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+
+    let args = message.content.split(" ").slice(1);
+
+    if (command === "game") {  
+        const embed = new Discord.RichEmbed()
+        .setColor(0xFFFFFF)
+        .setTitle("Organisation d'une partie :")
+        .setDescription(" **@agents** ")
+        client.channels.get("579282279176601610").send({embed})
+        .then(function (message) {
+            message.react('✅');
+        })
+    }
+
+    if (command === "lobby") {  
+        const embed = new Discord.RichEmbed()
+        .setColor(0xFFFFFF)
+        .setTitle("Lien steam : " + args.join(" ") + "\n\nParamètres de la partie :")
+        .setDescription("Carte : 1⃣  Pangée | 2⃣  Fractale | 3⃣  Continent | 4⃣ plateau d'Ile | 5⃣ Aléatoire");
+        client.channels.get("579282279176601610").send({embed})
+        .then(function (message) {
+            message.react('1⃣')
+            .then(() => message.react('2⃣'))
+            .then(() => message.react('3⃣'))
+            .then(() => message.react('4⃣'))
+            .then(() => message.react('5⃣'));
+        })
+    }
+
+        if (command === "lobby") {
+        const embed = new Discord.RichEmbed()
+        .setColor(0xFFFFFF)
+        .setDescription("Timer : 🕒 Dynamique | 🔥 90s Rapide | 🚎 120s Normal | 💤 150s Lent");
+        client.channels.get("579282279176601610").send({embed})
+        .then(function (message) {
+            message.react('🕒')
+            .then(() => message.react('🔥'))
+            .then(() => message.react('🚎'))
+            .then(() => message.react('💤'));
+        })    
+    }    
+
+    if (command === "lobby") {
+        const embed = new Discord.RichEmbed()
+        .setColor(0xFFFFFF)
+        .setDescription("Âge du monde : 🗻 Classique | 🌋 Nouveau");
+        client.channels.get("579282279176601610").send({embed})
+        .then(function (message) {
+            message.react('🗻')
+            .then(() => message.react('🌋'));
+        })
+    }   
+    
+    if (command === "lobby") {
+        const embed = new Discord.RichEmbed()
+        .setColor(0xFFFFFF)
+        .setDescription("Bans : valable si NombreDeVotes ≥ NombreDeJoueurs/2");
+        client.channels.get("579282279176601610").send({embed})
+    }   
+
+    if (command === "clear") {
+        if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES"))return
+        message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+            messagesDeleted = messages.array().length;
+          })
+    }
 })
